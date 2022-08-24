71e6c0c1beca829ca6f7c1347028a2c41f1922d3
tonymarsh <tonymarsh0331@gmail.com>
2022-08-25 00:04:09 +0800

    :beers: 在模块B中使用模块A中的实体类

---
+++ JDK9-ModuleB/src/main/java/test/TestJDKModule.java
@@ -0,0 +1,10 @@
+package test;
+
+import bean.UserInModuleA;
+
+public class TestJDKModule {
+    public static void main(String[] args) {
+        UserInModuleA tempUser = new UserInModuleA("张伟", "男");
+        System.out.println(tempUser.getUsername() + " " + tempUser.getSex());
+    }
+}
