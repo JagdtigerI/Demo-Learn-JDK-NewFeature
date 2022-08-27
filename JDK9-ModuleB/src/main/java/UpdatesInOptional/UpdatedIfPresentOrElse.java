424d6b44a8a7dfa9d9cfc6dca7cdd39c6463e270
tonymarsh <tonymarsh0331@gmail.com>
2022-08-27 15:41:56 +0800

    :art: Improve format of the code.

--- JDK9-ModuleB/src/main/java/UpdatesInOptional/UpdatedIfPresentOrElse.java
+++ JDK9-ModuleB/src/main/java/UpdatesInOptional/UpdatedIfPresentOrElse.java
@@ -5,10 +5,10 @@ import java.util.Optional;
 public class UpdatedIfPresentOrElse {
     public static void main(String[] args) {
         Optional<Integer> optional = Optional.of(1);
-        optional.ifPresentOrElse(x -> System.out.println("Value: " + x), () ->
-                System.out.println("Not Present."));
+        optional.ifPresentOrElse(x -> System.out.println("Value: " + x),
+                () -> System.out.println("Not Present."));
         optional = Optional.empty();
-        optional.ifPresentOrElse(x -> System.out.println("Value: " + x), () ->
-                System.out.println("Not Present."));
+        optional.ifPresentOrElse(x -> System.out.println("Value: " + x),
+                () -> System.out.println("Not Present."));
     }
 }
