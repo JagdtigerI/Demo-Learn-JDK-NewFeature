85de035f777d9453230a3a1dc02879974f1ee0a3
tonymarsh <tonymarsh0331@gmail.com>
2022-09-02 20:34:14 +0800

    :memo: 更新注释

--- JDK15/src/SealedClass/Demo.java
+++ JDK15/src/SealedClass/Demo.java
@@ -1,11 +1,12 @@
 package SealedClass;
 
 public class Demo {
-    //sealed修饰后，Person 类只能限定某⼏个类继承
-    //限定关键字 permits
-    //限定的⼦类需要使⽤final修改，即不允许再有⼦类
-    //如果孙⼦类也想要继承密封类，则按照上述步骤，修改代码即可
-    //取消密封类 :non-sealed
+    /**
+     * 引⼊了sealed class或interfaces。
+     * 这些class或者interfaces只允许被permits的class或者interface进⾏扩展和实现。
+     * 原则上子类需要是final或sealed class，以实现限制⼦类集的目的。
+     * 但子类也可以用non-sealed修饰，作为取消sealed机制的选项
+     */
     sealed class Person permits U1, U2, U3 {
     } //⽗类
 
@@ -15,12 +16,12 @@ public class Demo {
     final class U2 extends Person {
     } //⼦类2
 
+    non-sealed class U3 extends Person {
+    } //⼦类3
+
     class User1 extends U1 {
     } //孙⼦类1
 
-    class User2 extends U1 {
+    class User2 extends U3 {
     } //孙⼦类2
-
-    non-sealed class U3 extends Person {
-    } //⼦类3
 }
